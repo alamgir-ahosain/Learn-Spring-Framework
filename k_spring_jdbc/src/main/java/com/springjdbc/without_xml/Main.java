9b5e1d6842bfa3f46b193985fc43466b981a3433
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-09-09 16:13:23 +0600

    Main Runner Class

---
+++ k_spring_jdbc/src/main/java/com/springjdbc/without_xml/Main.java
@@ -0,0 +1,35 @@
+package com.springjdbc.without_xml;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.springjdbc.without_xml.dao.StudentDao;
+import com.springjdbc.without_xml.model.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(config.class);
+
+        StudentDao studentDao = context.getBean("beanStudentDao", StudentDao.class);
+
+      //  Student st = new Student();
+
+        // st.setId(11);
+        // st.setName("and up Alamgir");
+        // st.setEmail("alamgir@gmail.com");
+        // int res = studentDao.insertStudent(st);
+        // int res = studentDao.updateStudent(st);
+
+        // int res=studentDao.deleteStudent(11);
+        // System.out.println(res);
+        Student s=studentDao.getStudentById(11);
+        System.out.println(s);
+
+        List<Student> studentList=studentDao.getAllStudent();
+        System.out.println(studentList);
+
+        
+
+    }
+}
