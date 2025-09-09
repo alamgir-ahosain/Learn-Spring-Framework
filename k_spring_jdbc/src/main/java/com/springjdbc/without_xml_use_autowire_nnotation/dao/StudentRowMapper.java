f9178ced226422b96e1b534a94c77c8b8b74beb3
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-09-09 16:14:42 +0600

    Interfaces and implementations for DB access(Insert ,Update,Delete ,get)

---
+++ k_spring_jdbc/src/main/java/com/springjdbc/without_xml_use_autowire_nnotation/dao/StudentRowMapper.java
@@ -0,0 +1,25 @@
+package com.springjdbc.without_xml_use_autowire_nnotation.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.springjdbc.without_xml_use_autowire_nnotation.model.Student;
+
+
+class StudentRowMapper implements RowMapper<Student> {
+
+    @Override
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+        System.out.println("Mapping row number: " + rowNum);
+
+        Student student = new Student();
+        student.setId(rs.getInt(1));
+        student.setName(rs.getString(2));
+        student.setEmail(rs.getString(3));
+        return student;
+        // throw new UnsupportedOperationException("Unimplemented method 'mapRow'");
+    }
+
+}
