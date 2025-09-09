9b0b8298b951e4764829e5027c2cfbfc5c15708f
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-09-09 10:55:01 +0600

    Interfaces and implementations for DB access(Insert ,Update,Delete )

--- k_spring_jdbc/src/main/java/com/springjdbc/crud/dao/StudentDao.java
+++ k_spring_jdbc/src/main/java/com/springjdbc/crud/dao/StudentDao.java
@@ -3,6 +3,8 @@ package com.springjdbc.crud.dao;
 import com.springjdbc.crud.model.Student;
 
 public interface StudentDao {
-    public int insert(Student student);
+    public int insertStudent(Student student);
+    public int updateStudent(Student student);
+    public int deleteStudent(int id);
 
 }
--- k_spring_jdbc/src/main/java/com/springjdbc/crud/dao/StudentDaoImpl.java
+++ k_spring_jdbc/src/main/java/com/springjdbc/crud/dao/StudentDaoImpl.java
@@ -7,20 +7,33 @@ public class StudentDaoImpl implements StudentDao {
 
     private JdbcTemplate jdbcTemplate;
 
-    @Override
-    public int insert(Student student) {
-        String qString = "insert into studentTable values(?,?,?)";
-        int r = this.jdbcTemplate.update(qString, student.getId(), student.getName(), student.getEmail());
-        return r;
-        // throw new UnsupportedOperationException("Unimplemented method 'insert'");
-    }
-
     public JdbcTemplate getJdbcTemplate() {
         return jdbcTemplate;
     }
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertStudent(Student student) {
+        String qString = "insert into studentTable values(?,?,?)";
+        int r = this.jdbcTemplate.update(qString, student.getId(), student.getName(), student.getEmail());
+        return r;
+        // throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public int updateStudent(Student student) {
+        String qString = "update studentTable set name=?,email=? where id=?";
+        int r = this.jdbcTemplate.update(qString, student.getName(), student.getEmail(), student.getId());
+        return r;
+    }
+     @Override
+    public int deleteStudent(int id) {
+        String qString = "delete from studentTable where id=?";
+        int r = this.jdbcTemplate.update(qString,id);
+        return r;
     }
 
 }
